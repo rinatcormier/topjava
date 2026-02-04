9c3f670c9df737fe1959457c773247b9b101fad9
Rinat <rinat.zalyaletdinov@yandex.com>
2026-02-04 11:16:12 +0300

    Implement spring beans service and controller for meals

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,43 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.util.MealsUtil;
 
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.MealsUtil.createTo;
+import static ru.javawebinar.topjava.util.MealsUtil.getTos;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFound;
+
+@Service
 public class MealService {
 
-    private MealRepository repository;
+    private final MealRepository repository;
 
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Collection<MealTo> getAll(int userId) {
+        return getTos(repository.getAll(userId), MealsUtil.DEFAULT_CALORIES_PER_DAY);
+    }
+
+    public MealTo get(int id, int userId) {
+        return createTo(checkNotFound(repository.get(id, userId), id));
+    }
+
+    public MealTo create(Meal meal, int userId) {
+        return createTo(repository.save(meal, userId));
+    }
+
+    public void update(Meal meal, int userId) {
+        checkNotFound(repository.save(meal, userId), meal.getId());
+    }
+
+    public void delete(int id, int userId) {
+        checkNotFound(repository.delete(id, userId), id);
+    }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,51 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealTo;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
+import java.util.Collection;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkIsNew;
+
+@Controller
 public class MealRestController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Autowired
     private MealService service;
 
+    public Collection<MealTo> getAll() {
+        log.info("getAll");
+        return service.getAll(SecurityUtil.authUserId());
+    }
+
+    public MealTo get(int id) {
+        log.info("get {}", id);
+        return service.get(id, SecurityUtil.authUserId());
+    }
+
+    public MealTo create(Meal meal) {
+        log.info("create {}", meal);
+        checkIsNew(meal);
+        return service.create(meal, SecurityUtil.authUserId());
+    }
+
+    public void update(Meal meal, int id) {
+        log.info("update {}", meal);
+        assureIdConsistent(meal, id);
+        service.update(meal, SecurityUtil.authUserId());
+    }
+
+    public void delete(int id) {
+        log.info("delete {}", id);
+        service.delete(id, SecurityUtil.authUserId());
+    }
 }
